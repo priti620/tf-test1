1721dd176322ae51f365873d54682cad7f1a79b0
priti620 <121668165+priti620@users.noreply.github.com>
2023-07-31 12:25:40 +0530

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,36 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "3.63.0"
+    }
+  }
+ 
+}
+provider "google" {
+  # Configuration options
+  project = "anthos-installation"
+  region  = "us-west1"
+  zone    = "us-west1-a"
+  credentials  ="keys.json"
+}
+resource "google_compute_instance" "demo" {
+  name         = var.instance_name
+  machine_type = var.machine_type
+  zone         = var.gcp_zone
+
+  boot_disk {
+    initialize_params {
+      image = var.image
+    }
+  }
+
+  network_interface {
+    network = "default"
+
+    access_config {
+      // Ephemeral IP
+    }
+  }
+
+  
---
+++ variables.tf
@@ -0,0 +1,29 @@
+variable "gcp_project" {
+  description = "GCP project name"
+  default     = "anthos-installation"
+}
+
+variable "gcp_region" {
+  description = "GCP region, e.g. us-east1"
+  default     = "us-west1"
+}
+
+variable "gcp_zone" {
+  description = "GCP zone, e.g. us-east1-a"
+  default     = "us-west1-b"
+}
+
+variable "machine_type" {
+  description = "GCP machine type"
+  default     = "n1-standard-1"
+}
+
+variable "instance_name" {
+  description = "GCP instance name"
+  default     = "se-hangout-demo"
+}
+
+variable "image" {
+  description = "image to build instance from"
+  default     = "debian-cloud/debian-9"
+}
